5d9a142464b5558d55bfb5f100441d2c406f8147
mw <maximilian.weichart@icloud.com>
2024-03-16 15:23:54 +0100

    Gym env class

--- tetris_gymnasium/tetris.py
+++ tetris_gymnasium/tetris.py
@@ -1,5 +1,12 @@
+from typing import Any, SupportsFloat
+
 import numpy as np
 import random
+import gymnasium as gym
+from gymnasium.core import RenderFrame, ActType, ObsType
+from gymnasium.spaces import Discrete, Box
+
+from tetris_gymnasium.tetrominoes import STANDARD_TETROMINOES
 
 
 class TetrisBag:
@@ -24,49 +31,72 @@ class TetrisBag:
             self.shuffle_bag()
         return piece
 
-class Tetris:
-    piece_colors = [
-        (0, 0, 0),
-        (255, 255, 0),
-        (147, 88, 254),
-        (54, 175, 144),
-        (255, 0, 0),
-        (102, 217, 238),
-        (254, 151, 32),
-        (0, 0, 255)
-    ]
 
-    tetrominoes = [
-        np.array([[0, 0, 0, 0],
-                  [1, 1, 1, 1],
-                  [0, 0, 0, 0],
-                  [0, 0, 0, 0]]),
-        np.array([[2, 2],
-                  [2, 2]]),
-        np.array([[0, 3, 0],
-                  [3, 3, 3],
-                  [0, 0, 0]]),
-        np.array([[0, 4, 4],
-                  [4, 4, 0],
-                  [0, 0, 0]]),
-        np.array([[5, 5, 0],
-                  [0, 5, 5],
-                  [0, 0, 0]]),
-        np.array([[6, 0, 0],
-                  [6, 6, 6],
-                  [0, 0, 0]]),
-        np.array([[0, 0, 7],
-                  [7, 7, 7],
-                  [0, 0, 0]])
-    ]
-
-    def __init__(self, height=20, width=10):
-        self.active_tetromino = None
+class Tetris(gym.Env):
+    def __init__(self, render_mode=None, width=10, height=20, tetrominoes=STANDARD_TETROMINOES):
         self.height = height
         self.width = width
+        self.window_width = width * 100
+        self.window_height = height * 100
+        self.tetrominoes = tetrominoes
+        self.active_tetromino = None
+
+        self.observation_space = Box(low=0, high=len(self.tetrominoes), shape=(width, height), dtype=np.int8)
+
+        # Todo: unified structure / abstraction for actions
+        self.action_space = Discrete(6)
+
+        self.reward_range = (-2, 4)
+
+        assert render_mode is None or render_mode in self.metadata["render_modes"]
+        self.render_mode = render_mode
+
+        # """
+        # If human-rendering is used, `self.window` will be a reference
+        # to the window that we draw to. `self.clock` will be a clock that is used
+        # to ensure that the environment is rendered at the correct framerate in
+        # human-mode. They will remain `None` until human-mode is used for the
+        # first time.
+        # """
+        # self.window = None
+        # self.clock = None
+
         self.reset()
 
-    def reset(self):
+    def step(self, action: ActType) -> tuple[ObsType, SupportsFloat, bool, bool, dict[str, Any]]:
+        if self.active_tetromino is None:
+            self.spawn_tetromino()
+        else:
+            if action == 0:  # move left
+                if not self.check_collision(self.active_tetromino, self.x - 1, self.y):
+                    self.x -= 1
+            elif action == 1:  # move right
+                if not self.check_collision(self.active_tetromino, self.x + 1, self.y):
+                    self.x += 1
+            elif action == 2:  # rotate clockwise
+                if not self.check_collision(self.rotate(self.active_tetromino, True), self.x, self.y):
+                    self.active_tetromino = self.rotate(self.active_tetromino, True)
+            elif action == 3:  # rotate counterclockwise
+                if not self.check_collision(self.rotate(self.active_tetromino, False), self.x, self.y):
+                    self.active_tetromino = self.rotate(self.active_tetromino, False)
+            elif action == 4:  # hard drop
+                self.drop_active_tetromino()
+                self.place_active_tetromino()
+                return self.clear_filled_rows(), False
+            elif action == 5:  # fall down
+                if not self.check_collision(self.active_tetromino, self.x, self.y + 1):
+                    self.y += 1
+                else:
+                    self.place_active_tetromino()
+                    return self.clear_filled_rows(), False
+
+        if self.gameover:
+            return -2, True
+
+        return 0, False
+
+    def reset(self, *, seed: int | None = None, options: dict[str, Any] | None = None) -> tuple[ObsType, dict[str, Any]]:
+        # Initialize fresh board
         self.board = np.zeros((self.height, self.width), dtype=np.int8)
 
         # Create bag
@@ -78,14 +108,40 @@ class Tetris:
         self.x, self.y = (self.width // 2 - len(self.current_piece[0]) // 2, 0)
         self.gameover = False
 
-    def rotate(self, piece, clockwise=True):
-        return np.rot90(piece, k=(1 if clockwise else -1))
+    def render(self) -> RenderFrame | list[RenderFrame] | None:
+        if self.active_tetromino is not None:
+            tetromino_height, tetromino_width = self.active_tetromino.shape
+            view = self.board.copy()
+            view[self.y:self.y + tetromino_height, self.x:self.x + tetromino_width] += self.active_tetromino
+        else:
+            view = self.board
+
+        char_field = np.where(view == 0, '.', view.astype(str))
+        field_str = '\n'.join(''.join(row) for row in char_field)
+        print(field_str)
+        print("==========")
 
     def spawn_tetromino(self):
         self.active_tetromino = self.tetrominoes[self.bag.get_next_piece()]
         pff = self.active_tetromino.shape
         self.x, self.y = self.width // 2 - self.active_tetromino.shape[0] // 2, 0
         self.gameover = self.check_collision(self.active_tetromino, self.x, self.y)
+
+    def place_active_tetromino(self):
+        # # Create a mask for non-zero elements in the active Tetromino
+        # mask = self.active_tetromino > 0
+        #
+        # # Calculate the shape to identify range for height and width
+        # tetromino_height, tetromino_width = self.active_tetromino.shape
+        #
+        # # Create coordinate grids for the destination based on the mask
+        # destination_y, destination_x = np.ogrid[self.y:self.y + tetromino_height, self.x:self.x + tetromino_width]
+        #
+        # # Use the mask to place the Tetromino's non-zero values onto the board
+        # self.board[destination_y[mask], destination_x[mask]] = self.active_tetromino[mask]
+        tetromino_height, tetromino_width = self.active_tetromino.shape
+        self.board[self.y:self.y + tetromino_height, self.x:self.x + tetromino_width] += self.active_tetromino
+        self.active_tetromino = None
 
     def check_collision(self, tetromino, x, y):
         """
@@ -116,21 +172,13 @@ class Tetris:
         # No collision detected
         return False
 
-    def place_active_tetromino(self):
-        # # Create a mask for non-zero elements in the active Tetromino
-        # mask = self.active_tetromino > 0
-        #
-        # # Calculate the shape to identify range for height and width
-        # tetromino_height, tetromino_width = self.active_tetromino.shape
-        #
-        # # Create coordinate grids for the destination based on the mask
-        # destination_y, destination_x = np.ogrid[self.y:self.y + tetromino_height, self.x:self.x + tetromino_width]
-        #
-        # # Use the mask to place the Tetromino's non-zero values onto the board
-        # self.board[destination_y[mask], destination_x[mask]] = self.active_tetromino[mask]
-        tetromino_height, tetromino_width = self.active_tetromino.shape
-        self.board[self.y:self.y + tetromino_height, self.x:self.x + tetromino_width] += self.active_tetromino
-        self.active_tetromino = None
+    def rotate(self, piece, clockwise=True):
+        return np.rot90(piece, k=(1 if clockwise else -1))
+
+    def drop_active_tetromino(self):
+        # Todo: is there a more efficient way?
+        while not self.check_collision(self.active_tetromino, self.x, self.y + 1):
+            self.y += 1
 
     def clear_filled_rows(self):
         """
@@ -156,49 +204,29 @@ class Tetris:
 
         return num_filled
 
-    def drop_active_tetromino(self):
-        # Todo: is there a more efficient way?
-        while not self.check_collision(self.active_tetromino, self.x, self.y+1):
-            self.y += 1
+    # def close(self):
+    #     super().close()
+    #
+    # @property
+    # def unwrapped(self) -> Env[ObsType, ActType]:
+    #     return super().unwrapped
+    #
+    # @property
+    # def np_random(self) -> np.random.Generator:
+    #     return super().np_random
+    #
+    # def __str__(self):
+    #     return super().__str__()
+    #
+    # def __enter__(self):
+    #     return super().__enter__()
+    #
+    # def __exit__(self, *args: Any):
+    #     return super().__exit__(*args)
+    #
+    # def get_wrapper_attr(self, name: str) -> Any:
+    #     return super().get_wrapper_attr(name)
 
-
-    def step(self, action_id):
-        if self.active_tetromino is None:
-            self.spawn_tetromino()
-        else:
-            if action_id == 0: # move left
-                if not self.check_collision(self.active_tetromino, self.x-1, self.y):
-                    self.x -= 1
-            elif action_id == 1: # move right
-                if not self.check_collision(self.active_tetromino, self.x+1, self.y):
-                    self.x += 1
-            elif action_id == 2: # rotate clockwise
-                if not self.check_collision(self.rotate(self.active_tetromino, True), self.x, self.y):
-                    self.active_tetromino = self.rotate(self.active_tetromino, True)
-            elif action_id == 3: # rotate counterclockwise
-                if not self.check_collision(self.rotate(self.active_tetromino, False), self.x, self.y):
-                    self.active_tetromino = self.rotate(self.active_tetromino, False)
-            elif action_id == 4: # hard drop
-                self.drop_active_tetromino()
-                self.place_active_tetromino()
-            elif action_id == 5: # fall down
-                if not self.check_collision(self.active_tetromino, self.x, self.y+1):
-                    self.y += 1
-                else:
-                    self.place_active_tetromino()
-
-    def render(self):
-        if self.active_tetromino is not None:
-            tetromino_height, tetromino_width = self.active_tetromino.shape
-            view = self.board.copy()
-            view[self.y:self.y + tetromino_height, self.x:self.x + tetromino_width] += self.active_tetromino
-        else:
-            view = self.board
-
-        char_field = np.where(view == 0, '.', view.astype(str))
-        field_str = '\n'.join(''.join(row) for row in char_field)
-        print(field_str)
-        print("==========")
 
 # Create an instance of Tetris
 tetris_game = Tetris()
