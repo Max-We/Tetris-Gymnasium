4817cd82b46d47fc173a29635a9806d5ca8376de
Maximilian Weichart <maximilian.weichart@icloud.com>
2024-04-26 11:54:52 +0200

    Cleanup Wrapper

--- tetris_gymnasium/envs/__init__.py
+++ tetris_gymnasium/envs/__init__.py
@@ -5,16 +5,10 @@ Once registered, the environment can be created using the `gym.make` function.
 """
 from gymnasium import register
 
-from tetris_gymnasium.envs.tetris import Tetris, TetrisLin
+from tetris_gymnasium.envs.tetris import Tetris
 
 register(
     id="tetris_gymnasium/Tetris",
     entry_point="tetris_gymnasium.envs:Tetris",
-    max_episode_steps=10,
-)
-
-register(
-    id="tetris_gymnasium/TetrisLin",
-    entry_point="tetris_gymnasium.envs:TetrisLin",
     max_episode_steps=10,
 )
