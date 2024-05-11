f5431fd3400cd23f81c0f1e2b6a9461c0d759fd1
Maximilian Weichart <maximilian.weichart@icloud.com>
2024-05-11 11:05:48 +0200

    Fix wrapper position

--- examples/train_cnn.py
+++ examples/train_cnn.py
@@ -127,13 +127,13 @@ def make_env(env_id, seed, idx, capture_video, run_name):
     def thunk():
         if capture_video and idx == 0:
             env = gym.make(env_id, render_mode="rgb_array")
+            env = CnnObservation(env)
             env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
         else:
             env = gym.make(env_id)
         env = gym.wrappers.RecordEpisodeStatistics(env)
         env.action_space.seed(seed)
 
-        env = CnnObservation(env)
         env = gym.wrappers.FrameStack(env, 4)
 
         return env
