247020a4a83c829e5b2ccec815e098e16749933b
Maximilian Weichart <maximilian.weichart@icloud.com>
2024-07-05 13:29:15 +0200

    rm old wrapper

--- tetris_gymnasium/wrappers/action.py
+++ tetris_gymnasium/wrappers/action.py
@@ -14,158 +14,6 @@ from tetris_gymnasium.components.tetromino import Tetromino
 from tetris_gymnasium.envs import Tetris
 
 
-class GroupedActions(gym.ObservationWrapper):
-    """Observation wrapper that groups the actions into placements.
-
-    The action space is the width of the board times 4 (4 rotations).
-    """
-
-    def __init__(self, env: Tetris):
-        """Initializes the wrapper.
-
-        Args:
-            env: The environment to wrap.
-        """
-        super().__init__(env)
-        self.action_space = Discrete(env.unwrapped.width * 4)
-        self.observation_space = gym.spaces.Dict(
-            {
-                "board": Box(
-                    low=0,
-                    high=len(env.unwrapped.tetrominoes),
-                    shape=(
-                        env.unwrapped.width * 4,
-                        env.unwrapped.height_padded,
-                        env.unwrapped.width_padded,
-                    ),
-                    dtype=np.uint8,
-                ),
-                "holder": Box(
-                    low=0,
-                    high=len(self.env.unwrapped.pixels),
-                    shape=(
-                        self.unwrapped.padding,
-                        self.unwrapped.padding * self.env.unwrapped.holder.size,
-                    ),
-                    dtype=np.uint8,
-                ),
-                "queue": gym.spaces.Box(
-                    low=0,
-                    high=len(self.env.unwrapped.pixels),
-                    shape=(
-                        self.env.unwrapped.padding,
-                        self.env.unwrapped.padding * self.env.unwrapped.queue.size,
-                    ),
-                    dtype=np.uint8,
-                ),
-            }
-        )
-
-        self.legal_actions_mask = np.ones(self.action_space.n)
-
-    def observation(self, observation):
-        """Observation wrapper that groups the actions into placements.
-
-        Args:
-            observation: The observation to wrap. This is the board without the active tetromino.
-
-        Returns:
-            A dictionary containing the grouped board, holder and queue observations.
-        """
-        board_obs = observation["board"]
-        holder_obs = observation["holder"]
-        queue_obs = observation["queue"]
-
-        grouped_board_obs = []
-
-        t = self.env.unwrapped.active_tetromino
-        for x in range(self.env.unwrapped.width):
-            # reset position
-            x = self.env.unwrapped.padding + x
-
-            for r in range(4):
-                y = 0
-
-                # do rotation
-                t = self.env.unwrapped.rotate(t)
-
-                # hard drop
-                while not self.env.unwrapped.collision(t, x, y + 1):
-                    y += 1
-
-                # append to results
-                if not self.env.unwrapped.collision(t, x, y):
-                    grouped_board_obs.append(
-                        self.env.unwrapped.project_tetromino(t, x, y)
-                    )
-                else:
-                    # this happens when rotation was illegal and the tetromino wasn't dropped at all
-                    grouped_board_obs.append(np.ones_like(board_obs))
-                    self.legal_actions_mask[
-                        (x - self.env.unwrapped.padding) * 4 + r
-                    ] = 0
-
-        # concat the results
-        grouped_board_obs = np.array(grouped_board_obs)
-
-        return {
-            "board": grouped_board_obs.astype(np.uint8),
-            "holder": holder_obs.astype(np.uint8),
-            "queue": queue_obs.astype(np.uint8),
-        }
-
-    def step(self, action):
-        """Performs the action.
-
-        Args:
-            action: The action to perform.
-
-        Returns:
-            The observation, reward, game over, truncated, and info.
-        """
-        x = action // 4
-        r = action % 4
-
-        if self.legal_actions_mask[x * 4 + r] == 0:
-            # Do nothing action
-            observation, reward, game_over, truncated, info = self.env.step(
-                self.env.unwrapped.actions.no_op
-            )
-            return self.observation(observation), reward, game_over, truncated, info
-
-        new_tetromino = copy.deepcopy(self.env.unwrapped.active_tetromino)
-
-        # Set new x position
-        x += self.env.unwrapped.padding
-        # Set new rotation
-        for _ in range(r):
-            new_tetromino = self.env.unwrapped.rotate(new_tetromino)
-
-        # Apply rotation and movement (x,y)
-        self.env.unwrapped.x = x
-        self.env.unwrapped.active_tetromino = new_tetromino
-        observation, reward, game_over, truncated, info = self.env.step(
-            self.env.unwrapped.actions.hard_drop
-        )
-        return self.observation(observation), reward, game_over, truncated, info
-
-    def reset(
-        self, *, seed: "int | None" = None, options: "dict[str, Any] | None" = None
-    ) -> "tuple[dict[str, Any], dict[str, Any]]":
-        """Resets the environment.
-
-        Args:
-            seed: The seed to use for the random number generator.
-            options: The options to use for the environment.
-
-        Returns:
-            The observation and info.
-        """
-        self.legal_actions_mask = np.ones(self.action_space.n)
-        observation, info = self.env.reset(seed=seed, options=options)
-        return self.observation(observation), info
-
-
 class GroupedActionsVector(gym.ObservationWrapper):
     """Observation wrapper that groups the actions into placements.
 
@@ -184,43 +32,15 @@ class GroupedActionsVector(gym.ObservationWrapper):
             low=0,
             high=env.unwrapped.height * env.unwrapped.width,
             shape=(
-                4 * (
-                        # env.unwrapped.width * env.unwrapped.width + # height
-                        env.unwrapped.width # max_height
-#                         env.unwrapped.width # holes
+                4
+                * (
+                    # env.unwrapped.width * env.unwrapped.width + # height
+                    env.unwrapped.width  # max_height
+                    #                         env.unwrapped.width # holes
                 ),
             ),
             dtype=np.float32,
         )
-        # self.observation_space = gym.spaces.Dict(
-        #     {
-        #         "height": Box(
-        #             low=0,
-        #             high=env.unwrapped.height,
-        #             shape=(
-        #                 env.unwrapped.width,
-        #                 env.unwrapped.width,1
-        #             ),
-        #             dtype=np.uint8,
-        #         ),
-        #         "max_height": Box(
-        #             low=0,
-        #             high=env.unwrapped.height * env.unwrapped.width,
-        #             shape=(
-        #                 env.unwrapped.width,1
-        #             ),
-        #             dtype=np.uint8,
-        #         ),
-        #         "holes": Box(
-        #             low=0,
-        #             high=env.unwrapped.height * env.unwrapped.width,
-        #             shape=(
-        #                 env.unwrapped.width,1
-        #             ),
-        #             dtype=np.uint8,
-        #         ),
-        #     }
-        # )
 
         self.legal_actions_mask = np.ones(self.action_space.n)
 
@@ -239,7 +59,10 @@ class GroupedActionsVector(gym.ObservationWrapper):
     def calculate_height(self, board):
         """Calculate the height of each column in the board."""
         # Slicing the board to remove padding
-        board = board[0:-self.env.unwrapped.padding, self.env.unwrapped.padding:-self.env.unwrapped.padding]
+        board = board[
+            0 : -self.env.unwrapped.padding,
+            self.env.unwrapped.padding : -self.env.unwrapped.padding,
+        ]
 
         # Create a mask where board is not equal to 0
         mask = board != 0
@@ -256,7 +79,6 @@ class GroupedActionsVector(gym.ObservationWrapper):
 
         return height
 
-
     def calculate_max_height(self, height):
         """Calculate the maximum height among all columns."""
         return np.max(height)
@@ -264,7 +86,10 @@ class GroupedActionsVector(gym.ObservationWrapper):
     def calculate_holes(self, board):
         """Calculate the number of holes in the board."""
         board = copy.deepcopy(board)
-        board = board[0:-self.env.unwrapped.padding, self.env.unwrapped.padding:-self.env.unwrapped.padding]
+        board = board[
+            0 : -self.env.unwrapped.padding,
+            self.env.unwrapped.padding : -self.env.unwrapped.padding,
+        ]
 
         if np.all(board == np.ones_like(board)):
             return board.shape[0] * board.shape[1]
@@ -274,13 +99,15 @@ class GroupedActionsVector(gym.ObservationWrapper):
             row = 0
             while row < len(board) and col[row] == 0:
                 row += 1
-            num_holes += len([x for x in col[row + 1:] if x == 0])
+            num_holes += len([x for x in col[row + 1 :] if x == 0])
         return num_holes
 
     def get_bumpiness_and_height(self, board):
         board = np.array(board)
         mask = board != 0
-        invert_heights = np.where(mask.any(axis=0), np.argmax(mask, axis=0), self.env.unwrapped.height)
+        invert_heights = np.where(
+            mask.any(axis=0), np.argmax(mask, axis=0), self.env.unwrapped.height
+        )
         heights = self.env.unwrapped.height - invert_heights
         total_height = np.sum(heights)
         currs = heights[:-1]
@@ -310,6 +137,7 @@ class GroupedActionsVector(gym.ObservationWrapper):
         # Check collision using numpy element-wise operations.
         result = np.any(board_subsection[tetromino.matrix > 0] == 1)
         return np.any(board_subsection[tetromino.matrix > 0] == 1)
+
     def observation(self, observation):
         """Observation wrapper that groups the actions into placements.
 
@@ -343,10 +171,10 @@ class GroupedActionsVector(gym.ObservationWrapper):
                     y += 1
 
                 # append to results
-                xx = (x - self.env.unwrapped.padding) * 4 + (r+1)%4
+                xx = (x - self.env.unwrapped.padding) * 4 + (r + 1) % 4
                 if self.collision_with_frame(t, x, y):
                     self.legal_actions_mask[
-                        (x - self.env.unwrapped.padding) * 4 + (r+1)%4
+                        (x - self.env.unwrapped.padding) * 4 + (r + 1) % 4
                     ] = 0
                     grouped_board_obs.append(np.ones_like(board_obs))
                 elif not self.env.unwrapped.collision(t, x, y):
@@ -360,8 +188,12 @@ class GroupedActionsVector(gym.ObservationWrapper):
         # concat the results
         grouped_board_obs = np.array(grouped_board_obs)
 
-        height_obs = np.array([self.calculate_height(board) for board in grouped_board_obs])
-        max_height_obs = np.array([self.calculate_max_height(height) for height in height_obs])
+        height_obs = np.array(
+            [self.calculate_height(board) for board in grouped_board_obs]
+        )
+        max_height_obs = np.array(
+            [self.calculate_max_height(height) for height in height_obs]
+        )
         # holes_obs = np.array([self.calculate_holes(board) for board in grouped_board_obs])
         # # hb_obs = np.array([self.get_bumpiness_and_height(board) for board in grouped_board_obs])
         #
@@ -394,7 +226,9 @@ class GroupedActionsVector(gym.ObservationWrapper):
             # truncated = False
             # info = {"action_mask": self.legal_actions_mask}
             # return observations, reward, game_over, truncated, info
-            obs, reward, game_over, truncated, info = self.env.unwrapped.step(self.env.unwrapped.actions.no_op)
+            obs, reward, game_over, truncated, info = self.env.unwrapped.step(
+                self.env.unwrapped.actions.no_op
+            )
             reward = self.env.unwrapped.rewards.invalid_action
             info["action_mask"] = self.legal_actions_mask
             return self.observation(obs), reward, game_over, truncated, info
