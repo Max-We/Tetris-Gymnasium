191d120a40d25f9a8d5bba1cd2d698b814503daf
Maximilian Weichart <maximilian.weichart@icloud.com>
2024-10-18 15:41:08 +0200

    add mv test

--- tetris_gymnasium/envs/tetris_fn.py
+++ tetris_gymnasium/envs/tetris_fn.py
@@ -10,15 +10,20 @@ from jax import random
 from tetris_gymnasium.functional.core import (
     EnvConfig,
     State,
+    check_game_over,
     collision,
-    lock_active_tetromino,
     create_board,
     get_initial_x_y,
+    graviy_step,
     hard_drop,
-    check_game_over,
+    lock_active_tetromino,
 )
-from tetris_gymnasium.functional.queue import QueueFunction, create_bag_queue, bag_queue_get_next_element, \
-    CreateQueueFunction
+from tetris_gymnasium.functional.queue import (
+    CreateQueueFunction,
+    QueueFunction,
+    bag_queue_get_next_element,
+    create_bag_queue,
+)
 from tetris_gymnasium.functional.tetrominoes import Tetrominoes, get_tetromino_matrix
 
 
@@ -32,28 +37,30 @@ def step(
 ) -> Tuple[chex.PRNGKey, State, float, bool, dict]:
     x, y, rotation = state.x, state.y, state.rotation
     board = state.board
-    active_tetromino_matrix = get_tetromino_matrix(const, state.active_tetromino, rotation)
+    active_tetromino_matrix = get_tetromino_matrix(
+        const, state.active_tetromino, rotation
+    )
     lines_cleared = 0
 
     def move_left():
         return jax.lax.cond(
             ~collision(board, active_tetromino_matrix, x - 1, y),
             lambda: x - 1,
-            lambda: x
+            lambda: x,
         )
 
     def move_right():
         return jax.lax.cond(
             ~collision(board, active_tetromino_matrix, x + 1, y),
             lambda: x + 1,
-            lambda: x
+            lambda: x,
         )
 
     def move_down():
         return jax.lax.cond(
             ~collision(board, active_tetromino_matrix, x, y + 1),
             lambda: y + 1,
-            lambda: y
+            lambda: y,
         )
 
     def rotate_clockwise():
@@ -62,7 +69,7 @@ def step(
         return jax.lax.cond(
             ~collision(board, new_matrix, x, y),
             lambda: (new_rotation, new_matrix),
-            lambda: (rotation, active_tetromino_matrix)
+            lambda: (rotation, active_tetromino_matrix),
         )
 
     def rotate_counterclockwise():
@@ -71,58 +78,84 @@ def step(
         return jax.lax.cond(
             ~collision(board, new_matrix, x, y),
             lambda: (new_rotation, new_matrix),
-            lambda: (rotation, active_tetromino_matrix)
+            lambda: (rotation, active_tetromino_matrix),
         )
 
-    x = jax.lax.switch(action, [move_left, move_right, lambda: x, lambda: x, lambda: x, lambda: x, lambda: x])
-    y = jax.lax.switch(action, [lambda: y, lambda: y, move_down, lambda: y, lambda: y, lambda: y, lambda: hard_drop(
-        board, active_tetromino_matrix, x, y)])
-    rotation, active_tetromino_matrix = jax.lax.switch(action, [
-        lambda: (rotation, active_tetromino_matrix),
-        lambda: (rotation, active_tetromino_matrix),
-        lambda: (rotation, active_tetromino_matrix),
-        rotate_clockwise,
-        rotate_counterclockwise,
-        lambda: (rotation, active_tetromino_matrix),
-        lambda: (rotation, active_tetromino_matrix)
-    ])
+    x = jax.lax.switch(
+        action,
+        [move_left, move_right, lambda: x, lambda: x, lambda: x, lambda: x, lambda: x],
+    )
+    y = jax.lax.switch(
+        action,
+        [
+            lambda: y,
+            lambda: y,
+            move_down,
+            lambda: y,
+            lambda: y,
+            lambda: y,
+            lambda: hard_drop(board, active_tetromino_matrix, x, y),
+        ],
+    )
+    rotation, active_tetromino_matrix = jax.lax.switch(
+        action,
+        [
+            lambda: (rotation, active_tetromino_matrix),
+            lambda: (rotation, active_tetromino_matrix),
+            lambda: (rotation, active_tetromino_matrix),
+            rotate_clockwise,
+            rotate_counterclockwise,
+            lambda: (rotation, active_tetromino_matrix),
+            lambda: (rotation, active_tetromino_matrix),
+        ],
+    )
 
     # Check if the tetromino should be locked
-    should_lock = collision(board, active_tetromino_matrix, x, y + 1)
+    y_gravity = graviy_step(const, board, state.active_tetromino, rotation, x, y)
+    should_lock = y_gravity == y
 
     state = State(
-        board = board,
-        active_tetromino = state.active_tetromino,
-        rotation = rotation,
-        x = x,
-        y = y,
-        queue = state.queue,
-        queue_index = state.queue_index,
-        game_over = False,
-        score = state.score
+        board=board,
+        active_tetromino=state.active_tetromino,
+        rotation=rotation,
+        x=x,
+        y=y_gravity,
+        queue=state.queue,
+        queue_index=state.queue_index,
+        game_over=False,
+        score=state.score,
     )
 
     # If should lock or it's a hard drop, commit the tetromino
     new_state, new_key = jax.lax.cond(
         should_lock | (action == 6),
         lambda: place_active_tetromino(config, const, state, queue_fn, key),
-        lambda: (state, key)
+        lambda: (state, key),
     )
 
-    return key, new_state, new_state.score - state.score, new_state.game_over, {"lines_cleared": lines_cleared}
+    return (
+        key,
+        new_state,
+        new_state.score - state.score,
+        new_state.game_over,
+        {"lines_cleared": lines_cleared},
+    )
+
 
 def reset(
     tetromiones: Tetrominoes,
     key: chex.PRNGKey,
     config: EnvConfig,
-    create_queue_fn: CreateQueueFunction= create_bag_queue,
-    queue_fn: QueueFunction = bag_queue_get_next_element
+    create_queue_fn: CreateQueueFunction = create_bag_queue,
+    queue_fn: QueueFunction = bag_queue_get_next_element,
 ) -> Tuple[chex.PRNGKey, State]:
     board = create_board(config, tetromiones)
 
     key, subkey = random.split(key)
     queue, queue_index = create_queue_fn(config, key)
-    active_tetromino, queue, queue_index, key = queue_fn(config, queue, queue_index, key)
+    active_tetromino, queue, queue_index, key = queue_fn(
+        config, queue, queue_index, key
+    )
 
     x, y = get_initial_x_y(config, tetromiones, active_tetromino)
 
@@ -135,25 +168,34 @@ def reset(
         queue=queue,
         queue_index=queue_index,
         game_over=False,
-        score=jnp.uint8(0)
+        score=jnp.uint8(0),
     )
 
     return key, state
+
 
 def place_active_tetromino(
     config: EnvConfig,
     tetrominoes: Tetrominoes,
     state: State,
     queue_fn: QueueFunction,
-    key: chex.PRNGKey
+    key: chex.PRNGKey,
 ) -> Tuple[State, chex.PRNGKey]:
     # Commit the active tetromino
     new_board, reward = lock_active_tetromino(
-        config, tetrominoes, state.board, state.active_tetromino, state.rotation, state.x, state.y
+        config,
+        tetrominoes,
+        state.board,
+        state.active_tetromino,
+        state.rotation,
+        state.x,
+        state.y,
     )
 
     # Spawn a new tetromino
-    new_active_tetromino, new_queue, new_queue_index, new_key = queue_fn(config, state.queue, state.queue_index, key)
+    new_active_tetromino, new_queue, new_queue_index, new_key = queue_fn(
+        config, state.queue, state.queue_index, key
+    )
     new_x, new_y = get_initial_x_y(config, tetrominoes, new_active_tetromino)
     new_rotation = 0
 
@@ -171,7 +213,7 @@ def place_active_tetromino(
         queue=new_queue,
         queue_index=new_queue_index,
         game_over=game_over,
-        score=state.score + reward
+        score=state.score + reward,
     )
 
     return new_state, new_key
--- tetris_gymnasium/functional/core.py
+++ tetris_gymnasium/functional/core.py
@@ -16,6 +16,7 @@ class EnvConfig(NamedTuple):
     padding: int
     queue_size: int
 
+
 @chex.dataclass
 class State:
     board: chex.Array
@@ -29,32 +30,75 @@ class State:
     game_over: bool
     score: int
 
+
 # Utility functions
 def create_board(config: EnvConfig, tetrominoes: Tetrominoes) -> chex.Array:
     empty_board = jnp.zeros((config.height, config.width), dtype=jnp.uint8)
-    padded_board = jnp.pad(empty_board, ((0, config.padding), (config.padding, config.padding)), mode='constant', constant_values=tetrominoes.base_pixels[1])
+    padded_board = jnp.pad(
+        empty_board,
+        ((0, config.padding), (config.padding, config.padding)),
+        mode="constant",
+        constant_values=tetrominoes.base_pixels[1],
+    )
     return padded_board
 
-def get_initial_x_y(config: EnvConfig, tetrominoes: Tetrominoes, active_tetromino: int) -> Tuple[int, int]:
-    x = (config.width + config.padding * 2) // 2 - get_tetromino_matrix(tetrominoes, active_tetromino, 0).shape[1] // 2
+
+def get_initial_x_y(
+    config: EnvConfig, tetrominoes: Tetrominoes, active_tetromino: int
+) -> Tuple[int, int]:
+    x = (config.width + config.padding * 2) // 2 - get_tetromino_matrix(
+        tetrominoes, active_tetromino, 0
+    ).shape[1] // 2
     y = 0
     return x, y
+
 
 def collision(board: chex.Array, tetromino: chex.Array, x: int, y: int) -> chex.Array:
     h, w = tetromino.shape
     board_section = jax.lax.dynamic_slice(board, (y, x), (h, w))
     return jnp.any((board_section > 0) & (tetromino > 0))
 
-def project_tetromino(board: chex.Array, tetromino: chex.Array, x: int, y: int, tetromino_id: int) -> chex.Array:
-    update = jax.lax.dynamic_update_slice(jnp.zeros_like(board), tetromino * tetromino_id, (y, x))
+
+def project_tetromino(
+    board: chex.Array, tetromino: chex.Array, x: int, y: int, tetromino_id: int
+) -> chex.Array:
+    update = jax.lax.dynamic_update_slice(
+        jnp.zeros_like(board), tetromino * tetromino_id, (y, x)
+    )
     return board + update
 
+
 def score(config: EnvConfig, rows_cleared: int) -> jnp.uint8:
-    return jnp.uint8((rows_cleared ** 2) * config.width)
+    return jnp.uint8((rows_cleared**2) * config.width)
+
 
 # Core game logic functions
-def clear_filled_rows(config: EnvConfig, tetrominoes: Tetrominoes, board: chex.Array) -> Tuple[chex.Array, chex.Array]:
-    filled_rows = jnp.all(board[:, config.padding:-config.padding] > 0, axis=1)
+def graviy_step(
+    tetrominoes: Tetrominoes,
+    board: chex.Array,
+    active_tetromino: int,
+    rotation: int,
+    x: int,
+    y: int,
+) -> Tuple[chex.Array, int]:
+    new_y = jax.lax.cond(
+        ~collision(
+            board,
+            get_tetromino_matrix(tetrominoes, active_tetromino, rotation),
+            x,
+            y + 1,
+        ),
+        lambda: y + 1,
+        lambda: y,
+    )
+
+    return new_y
+
+
+def clear_filled_rows(
+    config: EnvConfig, tetrominoes: Tetrominoes, board: chex.Array
+) -> Tuple[chex.Array, chex.Array]:
+    filled_rows = jnp.all(board[:, config.padding : -config.padding] > 0, axis=1)
     n_filled = jnp.sum(filled_rows)
 
     def clear_rows(board):
@@ -86,14 +130,19 @@ def hard_drop(board: chex.Array, tetromino: chex.Array, x: int, y: int) -> int:
         return ~collision(board, tetromino, x, y + 1)
 
     def body_fun(y):
-        return y+1
+        return y + 1
 
     return jax.lax.while_loop(cond_fun, body_fun, y)
 
-def lock_active_tetromino(config: EnvConfig, tetrominoes: Tetrominoes, board, active_tetromino, rotation, x, y) -> Tuple[chex.Array, chex.Array]:
+
+def lock_active_tetromino(
+    config: EnvConfig, tetrominoes: Tetrominoes, board, active_tetromino, rotation, x, y
+) -> Tuple[chex.Array, chex.Array]:
     tetromino_matrix = get_tetromino_matrix(tetrominoes, active_tetromino, rotation)
     # place the tetromino on the board
-    updated_board = project_tetromino(board, tetromino_matrix, x, y, tetrominoes.ids[active_tetromino])
+    updated_board = project_tetromino(
+        board, tetromino_matrix, x, y, tetrominoes.ids[active_tetromino]
+    )
     # clear filled rows
     updated_board, lines_cleared = clear_filled_rows(config, tetrominoes, updated_board)
     # calculate reward
@@ -101,5 +150,10 @@ def lock_active_tetromino(config: EnvConfig, tetrominoes: Tetrominoes, board, ac
 
     return updated_board, reward
 
-def check_game_over(tetrominoes: Tetrominoes, board, active_tetromino, rotation, x, y) -> bool:
-    return collision(board, get_tetromino_matrix(tetrominoes, active_tetromino, rotation), x, y)
+
+def check_game_over(
+    tetrominoes: Tetrominoes, board, active_tetromino, rotation, x, y
+) -> bool:
+    return collision(
+        board, get_tetromino_matrix(tetrominoes, active_tetromino, rotation), x, y
+    )
